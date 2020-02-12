d7af5cf3ed9bb6fa2ae2ad9e34579f586b1ebce1
lucalucy0220 <42394961+lucalucy0220@users.noreply.github.com>
2020-02-12 11:10:41 +0800

    Add files via upload
    from bitbucket

---
+++ code_comments.py
@@ -0,0 +1,170 @@
+
+
+/**  addEventListener.js
+ * This file contains functions for scroll map features.
+ * These helper functions will get the scroll position and mouse position,
+ * and updated them when needed.
+ */
+
+
+/**  auto_click.js
+ * Helper function that links to start function when the button is clicked.
+ */
+
+
+/**  backgroundTracker.js
+ * Use background script to keep track of current domain
+ * If domain changes, recording will pause until manually resume
+ */
+
+
+ /**  breakState.js
+ * This contains the helper function for break state during the cycle,
+ * which will notify the users by sending the notification message.
+ */
+
+
+/**  prepare.html
+ * This file is the first page after the user clicks the button Begin. 
+ * It contains 5 Questions that we want to ask for the users. 
+ * After users complete the questions and click Submit,
+ * it'll navigate to popup.html
+ */
+
+
+ /**  clearDate.js
+ * Set the stored data array to null
+ */
+
+ /**  countTime.html
+ * This is the main file for the timer. 
+ * It contains three main parts, work, play, and cycle number. 
+ * The timer uses popup2.js script and navigates to timer.html after the button clicked.
+ */
+
+/**  createReport.js
+ * It contains the function to create simple heat map for cursor tracking.
+ */
+
+
+ /**  createScrollMap.js
+ * It contains the function to create scroll map for scroll tracking.
+ */
+
+ /**  creativity.html
+ * Creativity is one of the page in index.html.
+ */
+
+/**  entertainment.html
+ * Entertainment is one of the page in index.html.
+ */
+
+/**  index.html
+ * This file is the main page for the clickStream and viewStream.
+ */ 
+
+
+ /**  moment.js
+ * moment.js
+ * version: 2.9.0
+ * authors: Tim Wood, Iskren Chernev, Moment.js contributors
+ * license: MIT
+ * moment.js.com
+ */
+
+
+ /**  options.html
+ * It lists all the sites that are visited.
+ */ 
+
+ /**  others.html
+ * Others is one of the page in index.html.
+ */
+
+/**  pomodoroState.js
+ * It is th default break state from pomodoro package.
+ */
+
+
+/** popup.html
+ * This is the first page of our extenstion. 
+ * Whenn the button is clicked, it navigates to prepare.html
+ */
+
+
+/** popup.js
+ * This script for popup.html. 
+ */
+
+
+/** popup2.js
+ * This script for project.html. 
+ */ 
+
+/** popup3.js
+ * This script for brief.html. 
+ */ 
+
+/** popupTracker.html
+ * This is the page for 4 buttons for 
+ * creating and hiding reports of heatmap. 
+ */
+
+/** popupTracker.js
+ * This script for popupTracker.html. 
+ */
+
+/** popupURLTracker.html
+ * It contains on and off buttons for productivity mode. 
+ */
+
+/** brief.html
+ * It contains 5 Questions that we want to ask for the users after the work cycle. 
+ * After users complete the questions and click Submit,
+ * it'll navigate to popup.html which is the start of our extension
+ */
+
+
+
+ /**  productivity.html
+ * Productivity is one of the page in index.html.
+ * It shows the time you spent on each site.
+ */
+
+
+
+
+/**  readingandreference.html
+ * Reading & ReferenceError is one of the page in index.html.
+ */
+
+
+/**  removeEventListener.js
+ * get the intervalId stored in chrome local storage by 'addEventListener.js',
+ * clear the interval and that "recording" function will stop executing 
+ */
+
+
+
+/**  settings.html
+ * Settings is one of the page in index.html.
+ */
+
+
+/**  social_networking.html
+ * Social Networking is one of the page in index.html.
+ */
+
+
+/** stop_heatmap.js
+ * The event listener to stop tracking when the button is clicked.
+ */
+
+
+/** time.js
+ * It includes the function to update the current time.
+ */
+
+/** time.html
+ * This page contains all the buttons for heat map and scroll map
+ */
